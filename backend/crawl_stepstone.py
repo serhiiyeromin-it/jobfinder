863af27f207a6c42d9954fc04367db4eec4b0891
MrRobob <romansmirnoff@gmx.de>
2025-06-16 23:08:15 +0200

    die haben wahrscheinlich einen neuen scrapingschutz, die url ist jetzt richtig.

--- backend/crawl_stepstone.py
+++ backend/crawl_stepstone.py
@@ -16,19 +16,22 @@ def crawl_stepstone(keywords, location, radius):
     for page in range(1, 7):
 
         search_url = f"https://www.stepstone.de/jobs/{formatted_keywords_b}/in-{location}?radius={radius}&page={page}&sort=2&action=sort_publish&q={formatted_keywords_a}&searchOrigin=Resultlist_top-search" # Hier wird die URL für die jeweilige Seite erstellt
+        print (search_url) # Hier wird die URL ausgegeben, um zu sehen, welche Seite gerade abgefragt wird
         try:
-            response = requests.get(search_url, headers=headers, timeout=10) # Hier wird die Anfrage an die URL gesendet
+            response = requests.get(search_url, headers=headers, timeout=30) # Hier wird die Anfrage an die URL gesendet
+            print("Status Code:", response.status_code)
+            print(response.text[:5000])
             if response.status_code == 200: # Hier wird überprüft, ob die Anfrage erfolgreich war (Statuscode 200)
 
                 soup = BeautifulSoup(response.content, 'html.parser') # Hier wird der Inhalt der Seite mit BeautifulSoup geparsed
-                job_listings = soup.find_all('div', class_='res-urswt') # Hier werden alle Jobangebote auf der Seite gefunden
+                job_listings = soup.find_all('div', class_='res-4em2ed') # Hier werden alle Jobangebote auf der Seite gefunden
 
                 if job_listings:
                     for job in job_listings:
 
-                        title = job.find('div', class_='res-nehv70').text.strip() if job.find('div', class_='res-nehv70') else "Kein Titel verfügbar" # Hier wird der Titel des Jobs gefunden
-                        link = job.find('a', class_='res-1foik6i')['href'] if job.find('a', class_='res-1foik6i') else "Kein Link verfügbar" # Hier wird der Link zum Job gefunden
-                        company = job.find('span', class_='res-btchsq').text.strip() if job.find('span', class_='res-btchsq') else "Keine Firma angegeben" # Hier wird der Firmenname gefunden
+                        title = job.find('div', class_='res-ewgtgq').text.strip() if job.find('div', class_='res-ewgtgq') else "Kein Titel verfügbar" # Hier wird der Titel des Jobs gefunden
+                        link = job.find('a', class_='res-1cv93ld')['href'] if job.find('a', class_='res-1cv93ld') else "Kein Link verfügbar" # Hier wird der Link zum Job gefunden
+                        company = job.find('span', class_='res-du9bhi').text.strip() if job.find('span', class_='res-du9bhi') else "Keine Firma angegeben" # Hier wird der Firmenname gefunden
 
                         job_data = { # Hier wird ein Dictionary für den Job erstellt
                         'title': title,
