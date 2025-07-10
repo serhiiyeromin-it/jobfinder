e3098b04471ef6727183512f98d0dbc41e68c73b
MrRobob <romansmirnoff@gmx.de>
2025-07-10 22:54:31 +0200

    test(backend): fix

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,6 +5,10 @@ import mongomock
 with mock.patch("pymongo.MongoClient", new=lambda *a, **k: mongomock.MongoClient()):
     from server import collection as jobs_collection
 
+@pytest.fixture(autouse=True)
+def set_env_vars():
+    os.environ["BAA_API_KEY"] = "fake-key"
+
 @pytest.fixture()
 def client():
     from server import app
