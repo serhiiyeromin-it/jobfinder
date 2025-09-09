668f76472324cf0a6f7a2f55333df8236277e4b2
SerhiiYerom <serhii.yeromin@tn.techstarter.de>
2025-09-09 12:08:59 +0200

    update backups

---
+++ backup-mongo.ps1
@@ -0,0 +1,25 @@
+
+# backup-mongo.ps1
+param (
+    [string]$MONGO_URI = "mongodb+srv://USERNAME:PASSWORD@cluster0.fotipne.mongodb.net/job_database",
+    [string]$OUT_DIR = "./backups/job_database-$(Get-Date -Format yyyy-MM-dd_HH-mm)"
+)
+
+Write-Host "===> Creating backup in $OUT_DIR"
+
+# Делаем дамп
+mongodump --uri="$MONGO_URI" --out="$OUT_DIR" --verbose
+
+# Конвертируем BSON → JSON
+$src = Join-Path $OUT_DIR "job_database"
+$dst = "./backups"
+
+foreach ($file in Get-ChildItem $src -Filter *.bson) {
+    $jsonFile = Join-Path $dst ($file.BaseName + ".json")
+    bsondump $file.FullName > $jsonFile
+    Write-Host "Exported $($file.Name) to $jsonFile"
+}
+
+Write-Host "===> Backup finished!"
+
+
